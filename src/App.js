04947bafe46cae7c1c4f0d2e9ba83233b0d16e5c
Thiago <trabalho.bandeira@gmail.com>
2022-11-29 17:52:22 -0300

    add sorteio de palavras

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import Jogo from "./components/Jogo";
 import Letras from "./components/Letras";
 import Chute from "./components/Chute";
 import alfabeto from "./alfabeto";
-
-
-
+import palavras from "./palavras";
 
 function App() {
   const [inputDesligado, setInputDesigado] = useState(true)
-  const [letrasSelecionadas, setLetrasSelecionadas] = useState([])
+  const [letrasSelecionadas, setLetrasSelecionadas] = useState(alfabeto)
+  const [erros, setErros] = useState(0)
+  const [palavraSorteada, setPalavraSorteada] = useState([])
+  const [palavraSelecionada, setPalavraSelecionada] = useState([])
+  function jogar(){
+    sorteio()
+    setInputDesigado(false)
+    setLetrasSelecionadas([])
+  }
+
+  function sorteio(){
+    const indice = Math.floor(Math.random() * palavras.length)
+    const palavra = palavras[indice]
+    const palavraSeparada = palavra.split("")
+    setPalavraSelecionada(palavraSeparada)
+
+    let tracos = []
+    palavraSeparada.forEach(()=> tracos.push(" _"))
+    setPalavraSorteada(tracos)
+
+  }
+
+
   return (
     <div className="App">
 
-      <Jogo />
+      <Jogo jogar={jogar} erros={erros} palavraSorteada={palavraSorteada}/>
 
       <Letras letrasSelecionadas={letrasSelecionadas}/>
       
--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import imagem0 from "../assets/forca0.png"
+import imagem1 from "../assets/forca1.png"
+import imagem2 from "../assets/forca2.png"
+import imagem3 from "../assets/forca3.png"
+import imagem4 from "../assets/forca4.png"
+import imagem5 from "../assets/forca5.png"
+import imagem6 from "../assets/forca6.png"
 
 
-export default function Jogo() {
-      
+export default function Jogo({jogar, erros, palavraSorteada}) {
+    
+    const imagens = [imagem0,imagem1,imagem2,imagem3,imagem4,imagem5,imagem6]
+    
     return(
         <div className="jogo-main-container">
-            <img className="img-forca" src={imagem0} alt=''/>
+            <img className="img-forca" src={imagens[erros]} alt=''/>
             <div className="jogo-botao-palavra-container">
-                <button className="btn-iniciar" >Escolher Palavra</button>
+                <button className="btn-iniciar" onClick={jogar}>Escolher Palavra</button>
                 <div className="palavra-box">
-                <h1 class="">_ _ _ _</h1>
+                <h1 className="">{palavraSorteada}</h1>
                 </div>
             </div>
         </div>
